2669b78b29960c618971a9b5961ca021925b4f87
Renata França <renatacpf@gmail.com>
2025-04-25 22:00:46 -0300

    Conceitos Operadores Aritmeticos

---
+++ conceitos/operadores-aritmeticos.js
@@ -0,0 +1,30 @@
+/* 
+    +   adição
+    -   subtração
+    *   multiplicação
+    /   divisão
+    %   resto
+    **  exponencial
+*/
+
+// +   adição
+console.log( 1 + 1) // 2
+
+// -   subtração
+console.log( 10 - 7) // 3
+
+// *   multiplicação
+console.log( 2 * 2) // 4
+
+// /   divisão
+console.log( 11 / 2) // 5.5
+
+// %   resto
+console.log( 11 % 2) // 1
+
+// **  exponencial
+console.log( 4 ** 4) // 256
+
+// precedencia - ordem de execução / prioridade
+console.log( 1 + 1 * 2) // 3 - multiplicação, adição
+console.log((1 + 1) * 2) // 4 - adição, multiplicação ( dentro do parenteses, depois o que está fora)
