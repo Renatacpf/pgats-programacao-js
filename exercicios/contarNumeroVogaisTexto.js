638d1a5402e744a8b7565bc2280979863b84c27b
Renata França <renatacpf@gmail.com>
2025-04-26 18:16:26 -0300

    Exercicios Extras

---
+++ exercicios/contarNumeroVogaisTexto.js
@@ -0,0 +1,20 @@
+/* 
+Retorne o número de vogais no texto fornecido. Consideraremos a, e, i, o e u como vogais para 
+este Kata. O texto de entrada conterá apenas letras minúsculas e/ou espaços. Letras acentuadas 
+não fazem parte desse desafio.
+*/
+
+const contarVogais = (texto) => {
+    const vogais = ['a', 'e', 'i', 'o', 'u'];
+    return texto.split('').filter(letra => vogais.includes(letra)).length;
+  };
+  
+  // Exemplo de uso
+  const texto = "hoje o dia esta muito bonito e tranquilo";
+  const quantidadeDeVogais = contarVogais(texto);
+  
+  console.log("=== RESULTADO DA CONTAGEM ===");
+  console.log(`Texto analisado: "${texto}"`);
+  console.log(`Quantidade de vogais encontradas: ${quantidadeDeVogais}`);
+  console.log("=============================");
+  
