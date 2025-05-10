65a61551dd733a35606b992e33ccabfd1de8de07
Renata França <renatacpf@gmail.com>
2025-05-09 21:08:31 -0300

    Conceitos forEach

--- conceitos/repeticao-forEach.js
+++ conceitos/repeticao-forEach.js
@@ -19,16 +19,22 @@ listaDeAlunos.forEach(aluno => {
 })
 
 const listaDeAlunosENotas = [
-    { nome: 'Matheus', nota: 6.9 },
-    { nome: 'Renata', nota: 5 },
+    { nome: 'Matheus', nota: 10 },
+    { nome: 'Renata', nota: 9 },
     { nome: 'Leandro', nota: 8 },
     { nome: 'Ana', nota: 7 },
-    { nome: 'Lucas', nota: 6 }
+    { nome: 'Lucas', nota: notaDoLucas() }
 ]
+
+function notaDoLucas(){
+    return 1;
+}
+
+console.log(`--------------------------------`)
 
 listaDeAlunosENotas.forEach(aluno => {
     if (aluno.nota < 7) {
         console.log(`O aluno ${aluno.nome} precisa do trabalho extra`)
     }
-    console.log(`O aluno ${aluno.nome} tem a nota ${aluno.nota}`)
+    // console.log(`O aluno ${aluno.nome} tem a nota ${aluno.nota}`)
 })
