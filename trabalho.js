3d91b717827d78c2b38bc8e0e27a7f1d4fa120d1
Renata França <renatacpf@gmail.com>
2025-05-10 18:09:29 -0300

    Trabalho Final

---
+++ trabalho.js
@@ -0,0 +1,34 @@
+function geradorDeTagsDeIdentificacao(nome) {
+  return nome.toUpperCase();
+}
+
+function verificarSePodeSerAdotado(idade, porte) {
+  return idade >= 1 && porte === 'M';
+}
+
+function calcularConsumoDeRacao(nome, idade, peso) {
+  return peso * 300;
+}
+
+function decidirTipoDeAtividadePorPorte(porte) {
+  const atividades = {
+    pequeno: 'brincar dentro de casa',
+    medio: 'caminhadas no bairro',
+    grande: 'correr no parque'
+  };
+  return atividades[porte];
+}
+
+async function buscarDadoAsync() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve('Pipoca'), 100);
+  });
+}
+
+export  {
+  geradorDeTagsDeIdentificacao,
+  verificarSePodeSerAdotado,
+  calcularConsumoDeRacao,
+  decidirTipoDeAtividadePorPorte,
+  buscarDadoAsync
+};
